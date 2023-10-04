271e959c452ea8f49d995b7a40e67d871a8b2cd9
“BIPINsuthar” <bipinnsuthar@gmail.com>
2023-10-04 17:47:12 +0530

    clerk added for authentication

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="bg-light-400">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
