41ae32da34d85be371fc5cf7c7f74bcee5a66f38
“BIPINsuthar” <bipinnsuthar@gmail.com>
2023-10-14 13:41:24 +0530

    bug fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = "whsec_mkWHKq+0MtNTrfexkL9RJr0EBH6eNtiT";
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
