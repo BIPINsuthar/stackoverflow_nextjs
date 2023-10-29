dfa0ccdb71042e5b369876bb9290f530529ccbb3
“BIPINsuthar” <bipinnsuthar@gmail.com>
2023-10-29 10:30:55 +0530

    interaction modal added

---
+++ lib/actions/interaction.actions.ts
@@ -0,0 +1,38 @@
+"use server";
+import { connectToDatabase } from "../mongoose";
+import * as Models from ".././model";
+
+export async function viewQuestion(params: {
+  userId: string;
+  questionId: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const { questionId, userId } = params;
+
+    await Models.Question.findByIdAndUpdate(questionId, {
+      $inc: { views: 1 },
+    });
+
+    if (userId) {
+      const existingInteraction = await Models.Interaction.findOne({
+        user: userId,
+        action: "view",
+        question: questionId,
+      });
+
+      if (existingInteraction) return console.log("User has already viewed.");
+
+      //create interations
+
+      await Models.Interaction.create({
+        user: userId,
+        action: "view",
+        question: questionId,
+      });
+    }
+  } catch (error) {
+    throw error;
+  }
+}
--- lib/model/index.ts
+++ lib/model/index.ts
@@ -2,3 +2,4 @@ export * from "./question.model";
 export * from "./tag.modal";
 export * from "./user.modal";
 export * from "./answer.model";
+export * from "./interaction.model";
